877d0b39495b8828762d31b53b8541dda16dd9c7
Leonardo Felipe Silva <felipeleao217@gmail.com>
2025-12-07 19:44:44 -0300

    Limpando a branch main

--- src/model/Autenticacao.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Autenticacao {
-    boolean autenticar(String login, String senha);
-}
-
--- src/model/Relatorio.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-public interface Relatorio {
-    String gerarRelatorio();
-}
-
